27cfc827e3083a3fed16bbf7fa9e9c0aa92b2a51
rootKid <137388933+devMainak@users.noreply.github.com>
2024-07-12 06:00:50 +0000

    feat: created reducer

--- src/store.js
+++ src/store.js
@@ -1,7 +1,16 @@
 import { createStore } from 'redux'
 
-const financeReducer = () => {
-    
+const initialState = { income: 0, expenses: 0 }
+
+const financeReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case "ADD_INCOME":
+        return {...state, income: state.income + action.payload}
+      case "ADD_EXPENSES":
+        return {...state, expenses: state.expenses + action.paylaod }
+      default: 
+        return state
+    }
 }
 
 export default createStore(financeReducer)
